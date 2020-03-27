cc82dd34b0065336b0662e312e9ff9aae51d7816
Dom-888 <spinelladomenico88@gmail.com>
2020-03-27 15:27:29 +0000

    Edit comment

--- flask/run.py
+++ flask/run.py
@@ -5,11 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/') # The selected url (the root directory here) wil trigger the function below
 def hello():
     return "Hello World"
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=True) # Never leave it True when relasing an app
+            
